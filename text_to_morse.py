4dfeeee4623d7ef13065e2ccbb73474b4bdeaf3a
mattmanalang <matt.manalang@gmail.com>
2024-09-08 16:25:54 -0500

    Add method to write the Morse Code to a file

--- text_to_morse.py
+++ text_to_morse.py
@@ -1,4 +1,5 @@
 """Main file for the text-to-morse code program"""
+import datetime as dt
 
 class MorseCode:
     """Used for converting text including letters, numbers, and punctuation, into morse code"""
@@ -38,7 +39,20 @@ class MorseCode:
                 ciphertext += f"{MorseCode.MORSE_CHART.get(character.upper())} "
 
         return ciphertext.strip()
+    
+    def write_to_file(self, text):
+        """Writes string content to a file with the current time stamp as 'YYYY-MM-DD HH_MM UTC' as the file name."""
+        current_time_utc = dt.datetime.now(tz=dt.timezone.utc)
+        filename = f"{current_time_utc.year}-{current_time_utc.month}-{current_time_utc.day} "\
+            f"{current_time_utc.hour}_{current_time_utc.minute} UTC"
+        with open(f"{filename}.txt", "w", encoding="utf-8") as output:
+            output.write(text)
 
 
 morse = MorseCode()
-print(morse.to_morse("Hello world"))
+
+print("Allowed characters: Letters (A-Z), Numbers (0-9), Punctuation (. , ? / ( ) : = + - \' \" @)")
+user_msg = input("Enter message to be converted into Morse: ")
+ciphertext = morse.to_morse(user_msg)
+
+morse.write_to_file(ciphertext)
